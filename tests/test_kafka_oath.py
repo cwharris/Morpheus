31d58eff78bda3073997e1635a2a2dccdd05edc0
Christopher Harris <xixonia@gmail.com>
2023-08-29 19:38:10 +0000

    replace oauth mock server with ory hydra

---
+++ tests/test_kafka_oath.py
@@ -0,0 +1,29 @@
+
+from _utils import assert_results
+from morpheus.pipeline.linear_pipeline import LinearPipeline
+from morpheus.stages.input.kafka_source_stage import KafkaSourceStage
+from morpheus.stages.output.compare_dataframe_stage import CompareDataFrameStage
+
+import cudf
+
+def test_kafka_oath(config):
+    print("hello")
+
+    expected = cudf.DataFrame({ "a": "b" })
+
+    pipe = LinearPipeline(config)
+    pipe.set_source(
+        KafkaSourceStage(config,
+                         bootstrap_servers="kafka:9092",
+                         input_topic="morpheus_input_topic",
+                         auto_offset_reset="earliest",
+                         poll_interval="1seconds",
+                         client_id='morpheus_kafka_source_stage_pipe',
+                         stop_after=1))
+
+    comp_stage = pipe.add_stage(CompareDataFrameStage(config, expected))
+
+    pipe.run()
+
+    assert_results(comp_stage.get_results())
+
